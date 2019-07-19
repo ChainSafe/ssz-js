5d84e179ee598e5747c4b471ce489a7f133eb938
Marin Petrunić <marin.petrunic@gmail.com>
2019-07-19 14:31:14 +0200

    replace js-sha256 with wasm implementation

--- src/hash.ts
+++ src/hash.ts
@@ -1,9 +1,11 @@
 /** @module ssz */
-import { sha256 } from "js-sha256";
+import { Sha256 } from "sha256-rust-wasm";
 
 /**
  * Hash used for hashTreeRoot
  */
 export function hash(...inputs: Buffer[]): Buffer {
-  return Buffer.from(inputs.reduce((acc, i) => acc.update(i), sha256.create()).arrayBuffer());
+  //this will share memory instead of allocating new buffer
+  //https://nodejs.org/api/buffer.html#buffer_buffers_and_typedarray
+  return Buffer.from(inputs.reduce((acc, i) => acc.update(i), Sha256.create()).final().buffer);
 }
